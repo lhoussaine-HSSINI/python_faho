85ac0300ec6841212c9f08bf11c44bc634f265ad
lhoussaine HSSINI <61427735+lhoussaine-HSSINI@users.noreply.github.com>
2022-07-26 12:40:01 +0100

    v1

---
+++ 1.py
@@ -0,0 +1,15 @@
+ # variable
+nam="hssini"
+age="26"
+a=26
+b=True
+c="hssini""lhoussaine"
+print(c)
+print("my name is : " + nam)
+print("my age is : " + age)
+
+#day 1 print
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
